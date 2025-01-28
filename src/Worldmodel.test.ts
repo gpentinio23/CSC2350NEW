52ac546b3d6c442b3d3ee3534a0e51a7169cc041
gpentinio23 <gpentinio@lions.molloy.edu>
2025-01-28 16:25:09 -0500

    Update

--- src/Worldmodel.test.ts
+++ src/Worldmodel.test.ts
@@ -6,9 +6,11 @@ it("updates the y position correctly after being turned and moved given that it 
 let snake1 = new Snake(0,1);
 let myWorld = new Worldmodel(snake1);
 const numSquares4 = Math.floor(Math.random() * 10);
-snake1.turnRight();
 myWorld.update(numSquares4);
 expect(snake1.position.x).toBe(numSquares4)
+snake1.turnRight();
+myWorld.update(numSquares4);
+expect(snake1.position.x).toBe(0)
 });
 
 
--- src/Worldmodel.ts
+++ src/Worldmodel.ts
@@ -4,19 +4,19 @@ import Snake from "./Snake";
 
 class WorldModel{
 	private snakeone: Snake;
-	constructor (){
-		this.snakeone = new Snake();
+	constructor (snake){
+		this.snake = snake;
 	}
 	/**Update method to move the snake
 	 * @param steps - the number of squares to move the snake
 	 */
 	public update(steps:number){
-		this.snakeone.move(steps);
+		this.snake.move(steps);
 	}
 	/**Retrieves the snake
 	 */
 	public getSnake(): Snake {
-		return this.snakeone;
+		return this.snake;
 	} 
 }
 
